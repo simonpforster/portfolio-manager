aa200ffbe1fbbcb4b36edb4b949a654d80c7f62f
simonpforster <simon.forster98@gmail.com>
2025-04-17 09:35:41 +0100

    create owners endpoints

--- src/project_router/mod.rs
+++ src/project_router/mod.rs
@@ -5,16 +5,23 @@ use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
 use std::sync::Arc;
+use tracing::info;
 
 pub(crate) fn router(state: Arc<AppState>) -> Router {
     Router::new()
         .route("/projects/{document_id}", get(get_handler))
-        .route("/owners/{owner_id}/projects", get(get_handler)) // get all projects for an owner
-        .route("/owners/{owner_id}/projects/{document_id}", get(get_handler)) // get a specific project by project id from an owner
+        .route("/owners/{owner_id}/projects", get(get_projects_for_owner_handler)) // get all projects for an owner
         .with_state(state)
 }
 
 async fn get_handler(Path(document_id): Path<String>, State(state): State<Arc<AppState>>) -> impl IntoResponse {
     let project: Project = state.project_repository.get_project_by_document_id(&document_id).await.unwrap();
+    info!("Project fetched: {:?}", project);
     axum::response::Json(project)
+}
+
+async fn get_projects_for_owner_handler(Path(owner_id): Path<String>, State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    let projects: Vec<Project> = state.project_repository.get_projects_by_owner(&owner_id).await;
+    info!("Projects fetched for owner: {:?}", projects);
+    axum::response::Json(projects)
 }
